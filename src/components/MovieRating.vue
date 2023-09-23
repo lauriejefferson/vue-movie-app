af7cde793c3902a92e0b20c92f990ac3e6f781da
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-09-23 17:42:32 -0400

    add watch for changes to userRating array

--- src/components/MovieRating.vue
+++ src/components/MovieRating.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import Rating from 'primevue/rating';
 import { useMovieStore } from '../stores/MovieStore';
 
@@ -13,19 +13,23 @@ const currMovies = ref([]);
 const initalValue = 0;
 let reducer = 0;
 let totalRating = 0;
-console.log("Current movie id: ", props.id)
-currMovies.value = movieStore.userRating.filter(userRating => userRating.movieId === props.id)
-console.log("Current movie: ", currMovies.value)
+let avgRating = 0;
 
-// function getAverageRating () {
-//     if (props.movieId === movieStore.userRating.movieId) {
-//         reducer = (acc, curr) => acc + parseInt(curr.rating)
-//         totalRating = movieStore.userRating.reduce(reducer, initalValue)
-//         console.log("Total rating ", totalRating)
-//         return Math.round(totalRating / movieStore.userRating.length);
-//     }
-// }
-// value.value = getAverageRating()
+function getAverageRating() {
+  currMovies.value = movieStore.userRating.filter(userRating => userRating.movieId === props.id)
+  reducer = (acc, curr) => acc + parseInt(curr.rating)
+  totalRating = currMovies.value.reduce(reducer, initalValue)
+  value.value = Math.round(totalRating / currMovies.value.length)
+}
+
+onMounted(() => {
+    getAverageRating()
+})
+
+watch(movieStore.userRating, () => {
+    getAverageRating()
+});
+
 </script>
 
 <template>
