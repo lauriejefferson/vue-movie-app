2b7372c159fc7d506a6fb0dfb164739b02d72774
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2025-03-16 18:49:00 -0400

    fixes styling on login and signup form pages and adds router link from login to signup page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,27 +3,26 @@
 </script>
 <template>
     <div class="container">
-     <h1>Login</h1>
-     <form action="#">
-        <input type="email" placeholder="Enter your email"> 
-        <input type="email" placeholder="Enter your password">
-        <div class="btn">
-        <button>Log in</button> 
-        <button>Sign up</button> 
-        </div>
-        
-     </form>
+        <h1>Welcome Back!</h1>
+        <form action="#">
+            <input type="email" placeholder="Enter your email">
+            <input type="email" placeholder="Enter your password">
+            <button>Log in</button>
+        </form>
+        <p>Don't have an account? <router-link to="/signup">Sign up here.</router-link></p>
     </div>
 </template>
 
 
 
 <style scoped>
+h1 {
+    text-align: center;
+}
+
 form {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
+    display: grid;
+    place-items: center;
     width: 450px;
 }
 
@@ -34,19 +33,14 @@ input {
     border: 1px solid #C9C9CF;
     width: 100%;
 }
-.btn {
-    display: flex;
-    justify-content: space-between;
-    gap: 0.25em;
-    width: 100%;
-}
+
 button {
     font-size: 1.125rem;
     text-transform: uppercase;
     border: none;
     background-color: #5465FF;
     color: white;
-    width: 50%;
+    width: 100%;
     height: 45px;
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,20 +2,20 @@
 import { useMovieStore } from '../stores/MovieStore';
 import { useLocalStorage, useStorage } from '@vueuse/core';
 
-async function handleSubmit(values){
+async function handleSubmit(values) {
     console.log(values)
-// const state = useLocalStorage('signup', {email: values.email, password: values.password})
+    // const state = useLocalStorage('signup', {email: values.email, password: values.password})
 }
 
 </script>
 
 <template>
     <div class="container">
-    <h1>Sign Up</h1>
+        <h1>Sign Up</h1>
         <form action="#" @submit.prevent="handleSubmit">
-        <input type="email" name="email" placeholder="Enter your email">
-        <input type="password" name="password" placeholder="Enter your password">
-        <button>Sign Up</button>
+            <input type="email" name="email" placeholder="Enter your email">
+            <input type="password" name="password" placeholder="Enter your password">
+            <button>Sign Up</button>
         </form>
     </div>
 </template>
@@ -23,14 +23,15 @@ async function handleSubmit(values){
 
 
 <style scoped>
+h1 {
+    text-align: center;
+}
 
 form {
+    display: grid;
+    place-items: center;
     width: 400px;
     margin-top: 1.5em;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
 }
 
 input {
@@ -39,7 +40,7 @@ input {
     padding-left: 0.5em;
     border: 1px solid #C9C9CF;
     width: 100%;
-    
+
 }
 
 button {
